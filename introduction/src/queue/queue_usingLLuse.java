4a0a374e77eb4b621fbffd02a831d50ce4c9c8c3
Abhishek Sharma <sharmaash876@gmail.com>
2019-12-28 17:52:02 +0530

    queue using linked list

---
+++ introduction/src/queue/queue_usingLLuse.java
@@ -0,0 +1,19 @@
+package queue;
+
+public class queue_usingLLuse {
+
+	public static void main(String[] args) throws queue_empty_exception
+	{
+		QueueUsingLL<Integer> obj = new QueueUsingLL<>();
+		for(int i=0;i<5;i++)
+		{
+			obj.enqueue(i);
+		}
+		
+		while(obj.size()!=0)
+		{
+			System.out.println(obj.dequeue());
+		}
+	}
+
+}
