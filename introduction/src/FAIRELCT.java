5d66e02ac450806ab120d252896737f656cb863e
Abhishek Sharma <sharmaash876@gmail.com>
2021-01-02 00:15:00 +0530

    CodeChef question solution

---
+++ introduction/src/FAIRELCT.java
@@ -0,0 +1,73 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FAIRELCT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		while(t-->0)
+		{
+			int n = s.nextInt();
+			int m = s.nextInt();
+			
+			long a[] = new long[n];
+			long b[] = new long[m];
+			
+			long sumA = 0;
+			long sumB = 0;
+			
+			for(int i = 0 ; i < n ; i++)
+			{
+				a[i] = s.nextInt();
+				sumA += a[i];
+			}
+			
+			for(int i = 0 ; i < m ; i++)
+			{
+				b[i] = s.nextInt();
+				sumB += b[i];
+			}
+			
+			Arrays.sort(a);
+			Arrays.sort(b);
+			
+			int ai = 0;
+			int bi = m-1;
+			
+			int finalAns = 0;
+			
+			while(sumB>sumA && ai < n && bi > 0 )
+			{
+				sumA = sumA - a[ai] + b[bi]; 
+				sumB = sumB + a[ai] - b[bi];
+				long temp = a[ai];
+				a[ai] = b[bi];
+				b[bi] = temp;
+				ai++;
+				bi--;
+				finalAns++;
+//				System.out.println("ai: " + ai);
+//				System.out.println("bi: " + bi);
+//				System.out.println(sumA);
+//				System.out.println(sumB);
+			}
+			
+			if(sumB>=sumA)
+			{
+				System.out.println(-1);
+			}
+			else
+			{
+				System.out.println(finalAns);
+			}
+			
+		}
+       
+        s.close();
+
+	}
+
+}
