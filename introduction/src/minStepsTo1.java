fe580189e9e43db0917258f73ac78d1feb25b691
Abhishek Sharma <sharmaash876@gmail.com>
2021-01-08 16:19:31 +0530

    min steps to 1 DP solution

--- introduction/src/minStepsTo1.java
+++ introduction/src/minStepsTo1.java
@@ -69,11 +69,42 @@ public class minStepsTo1 {
 		
 		return storage[n];
 	}
+	
+	public static int minStepFuncDP(int n)
+	{
+		int[] storage = new int[n+1];
+		storage[1] = 0;
+		
+		for(int i = 2 ; i <= n ; i++)
+		{
+			int min = storage[i-1];
+			
+			if(i%3 == 0)
+			{
+				if(min>storage[i/3])
+				{
+					min = storage[i/3];
+				}
+			}
+			
+			if(i%2 == 0)
+			{
+				if(min>storage[i/2])
+				{
+					min = storage[i/2];
+				}
+			}
+			
+			storage[i] = min + 1;
+		}
+		return storage[n];
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n = 4; 
+		int n = 999; 
 		int[] arr = new int[n+1];
+		System.out.println(minStepFuncDP(n));
 		System.out.println(minStepFuncM(n , arr));
 		System.out.println(minStepFunc(n));
 
